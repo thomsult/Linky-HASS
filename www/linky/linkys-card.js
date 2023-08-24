71c7876d8a7dd9de6f029c1b1976a138c2bd1ea7
Thomas Sultan <thomsult@gmail.com>
2023-08-24 12:11:19 +0200

    update js

--- www/linky/linkys-card.js
+++ www/linky/linkys-card.js
@@ -1,4 +1,14 @@
-"use strict";
+// @ts-nocheck
+import "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js";
+
+function formatDate(date){
+    const d = new Date(date);
+    const formatNumber = (n) => (n > 10 ? n : 0 + n.toString());
+
+    return `${formatNumber(d.getHours())}:${formatNumber(d.getMinutes())}`;
+};
+
+
 class HelloWorldCard extends HTMLElement {
     config;
     content;
@@ -9,59 +19,59 @@ class HelloWorldCard extends HTMLElement {
     setConfig(config) {
         this.config = config;
     }
+
+    createChart(ctx, data, label) {
+        if (!ctx || !data) {
+            return;
+        }
+        return new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: data.map((e) => formatDate(e.date)),
+                datasets: [
+                    {
+                        label,
+                        data: data.map((e) => e.value),
+                        borderWidth: 1,
+                        backgroundColor: "rgba(255, 99, 132, 0.8)",
+                        borderColor: "rgba(255, 99, 132, 1)",
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    // @ts-ignore
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    }
+
+
     set hass(hass) {
+
         const entityId = this.config.entity;
-        const state = hass.states[entityId];
-        const { attribution, friendly_name } = state.attributes;
-        if (!this.content && this.shadowRoot) {
+        this.content = hass.states[entityId];
+        const { attribution, friendly_name } = this.content.attributes;
+        if (this.content && this.shadowRoot) {
             this.shadowRoot.innerHTML = `
       <div 
       style="width: 100%; height: 100%; display: flex; align-items: center; padding: 16px; box-sizing: border-box; min-height: 50px;"
-      
       >
           <canvas 
-          style="width: 100%; height: auto;max-height:500px;  background-color: #FFF; border-radius: 4px;"
+          style="width: 100%; height: auto;max-height:500px;  background-color: transparent; border-radius: 4px;"
           id="linkys-graph"></canvas>
         </div>
       `;
+
             const ctx = this.shadowRoot.getElementById("linkys-graph");
-            if (!ctx)
-                return;
-            // @ts-ignore
-            import(
-            // @ts-ignore
-            "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js")
-                .then((e) => {
-                console.log(attribution);
-                new Chart(ctx, {
-                    type: "bar",
-                    data: {
-                        labels: attribution.map((e) => new Date(e.date).getHours() +
-                            "h" +
-                            new Date(e.date).getMinutes()),
-                        datasets: [
-                            {
-                                label: "Watts",
-                                data: attribution.map((e) => e.value),
-                                borderWidth: 1,
-                                backgroundColor: "rgba(255, 99, 132, 0.8)",
-                                borderColor: "rgba(255, 99, 132, 1)",
-                            },
-                        ],
-                    },
-                    options: {
-                        scales: {
-                            // @ts-ignore
-                            y: {
-                                beginAtZero: true,
-                            },
-                        },
-                    },
-                });
-            })
-                .catch((error) => console.error(error));
-            this.content = true;
+            const chart = this.createChart(ctx, attribution, friendly_name);
+
         }
+
     }
+
 }
 customElements.define("linkys-card", HelloWorldCard);
